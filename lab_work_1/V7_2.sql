2e84dd166162c721d6d6046d1432a4093bf10c42
Aliaksei <alexx333by@protonmail.com>
2019-09-18 00:47:31 +0300

    2.2 implemented

--- lab_work_1/V7_2.sql
+++ lab_work_1/V7_2.sql
@@ -5,3 +5,7 @@ GO
 SELECT COUNT(*) AS DepartmentCount from HumanResources.Department 
     where GroupName = 'Executive General and Administration';
 GO
+
+--2
+SELECT TOP 5 BusinessEntityID, JobTitle, Gender, BirthDate FROM HumanResources.Employee ORDER BY BirthDate DESC;
+GO
