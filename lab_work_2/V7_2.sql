cc886bae66eae8d8fe9b93ae8415c45a72815a8f
Aliaksei <alexx333by@protonmail.com>
2019-09-18 18:59:50 +0300

    Removed default DATETIME value from 2.2

--- lab_work_2/V7_2.sql
+++ lab_work_2/V7_2.sql
@@ -7,7 +7,7 @@ CREATE TABLE dbo.PersonPhone
     BusinessEntityID INT,
     PhoneNumber Phone,
     PhoneNumberTypeID INT,
-    ModifiedDate DATETIME DEFAULT (GETDATE())
+    ModifiedDate DATETIME)
 );
 GO
 
