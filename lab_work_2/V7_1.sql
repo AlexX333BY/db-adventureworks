7b4eb1f442e2db51c57a51cbbced8280cf5d4acf
Aliaksei <alexx333by@protonmail.com>
2019-09-18 13:47:10 +0300

    Some styling applied

--- lab_work_2/V7_1.sql
+++ lab_work_2/V7_1.sql
@@ -2,17 +2,31 @@ USE AdventureWorks2012
 GO
 
 -- 1
-SELECT HumanResources.EmployeePayHistory.BusinessEntityID, JobTitle, MAX(RateChangeDate) AS LastChangeDate 
-    FROM HumanResources.Employee INNER JOIN HumanResources.EmployeePayHistory 
-        ON (HumanResources.Employee.BusinessEntityId = HumanResources.EmployeePayHistory.BusinessEntityId) 
-GROUP BY HumanResources.EmployeePayHistory.BusinessEntityID, JobTitle
+SELECT
+    EmployeePayHistory.BusinessEntityID,
+    JobTitle,
+    MAX(RateChangeDate) AS LastChangeDate
+FROM
+    HumanResources.Employee
+    INNER JOIN HumanResources.EmployeePayHistory
+        ON (Employee.BusinessEntityId = EmployeePayHistory.BusinessEntityId)
+GROUP BY
+    EmployeePayHistory.BusinessEntityID,
+    JobTitle
 GO
 
 -- 2
-SELECT Employee.BusinessEntityID, JobTitle, Department.Name AS DepName, StartDate, EndDate, 
-    DATEDIFF(year, StartDate, ISNULL(EndDate, GETDATE())) AS Years FROM HumanResources.Employee 
-        INNER JOIN HumanResources.EmployeeDepartmentHistory 
-            ON (Employee.BusinessEntityID = EmployeeDepartmentHistory.BusinessEntityID) 
-        INNER JOIN HumanResources.Department 
-            ON (EmployeeDepartmentHistory.DepartmentID = Department.DepartmentID)
+SELECT
+    Employee.BusinessEntityID,
+    JobTitle,
+    Department.Name AS DepName,
+    StartDate,
+    EndDate,
+    DATEDIFF(year, StartDate, ISNULL(EndDate, GETDATE())) AS Years
+FROM
+    HumanResources.Employee
+    INNER JOIN HumanResources.EmployeeDepartmentHistory
+        ON (Employee.BusinessEntityID = EmployeeDepartmentHistory.BusinessEntityID)
+    INNER JOIN HumanResources.Department
+        ON (EmployeeDepartmentHistory.DepartmentID = Department.DepartmentID)
 GO
