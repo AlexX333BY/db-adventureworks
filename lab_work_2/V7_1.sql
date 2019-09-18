d188e068d911ded3432ff6c1c2d09ebe1c953bd6
Aliaksei <alexx333by@protonmail.com>
2019-09-18 13:42:08 +0300

    2.1.2 implemented

--- lab_work_2/V7_1.sql
+++ lab_work_2/V7_1.sql
@@ -1,7 +1,18 @@
 USE AdventureWorks2012
+GO
 
 -- 1
 SELECT HumanResources.EmployeePayHistory.BusinessEntityID, JobTitle, MAX(RateChangeDate) AS LastChangeDate 
     FROM HumanResources.Employee INNER JOIN HumanResources.EmployeePayHistory 
         ON (HumanResources.Employee.BusinessEntityId = HumanResources.EmployeePayHistory.BusinessEntityId) 
 GROUP BY HumanResources.EmployeePayHistory.BusinessEntityID, JobTitle
+GO
+
+-- 2
+SELECT Employee.BusinessEntityID, JobTitle, Department.Name AS DepName, StartDate, EndDate, 
+    DATEDIFF(year, StartDate, ISNULL(EndDate, GETDATE())) AS Years FROM HumanResources.Employee 
+        INNER JOIN HumanResources.EmployeeDepartmentHistory 
+            ON (Employee.BusinessEntityID = EmployeeDepartmentHistory.BusinessEntityID) 
+        INNER JOIN HumanResources.Department 
+            ON (EmployeeDepartmentHistory.DepartmentID = Department.DepartmentID)
+GO
